8f3def1f82aa014153fde720400d120b66467776
Ehor Meschenko <meshhenko04@bk.ru>
2024-02-26 23:15:54 +0300

    create a new project(taskmanager)

---
+++ taskmanager/main/forms.py
@@ -0,0 +1,16 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title","task"]
+        widgets = {"title": TextInput(attrs={
+            'class':'form-control',
+            'placeholder': 'input name'
+        }),
+            "task": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'input text'
+            })
+        }
